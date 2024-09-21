1dcf8b5c4e6736da6240e46256b28c6549281567
Juan Sormany Da Silva <juansormany1000@gmail.com>
2024-09-21 19:07:32 -0300

    att

--- Aulajava03/src/aulacasting/main.java
+++ Aulajava03/src/aulacasting/main.java
@@ -3,12 +3,12 @@ package aulacasting;
 public class main {
 
 	public static void main(String[] args) {
-		int x;
-		double y;
-		x = 5;
-		y = 2 * x;
-		System.out.println(x);
-		System.out.println(y);
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
 	}
 
 }
